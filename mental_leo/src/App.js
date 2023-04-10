d5c1440152c410ec508de32467dfc90907b7fd03
Biyatra Ghosh <biyatraghosh13@gmail.com>
2023-04-10 15:32:19 +0530

    10 apr edit

--- mental_leo/src/App.js
+++ mental_leo/src/App.js
@@ -1,5 +1,6 @@
-
+import React from 'react';
 import './App.css';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Homepage/Navbar';
 import Placeholder from './Homepage/placeholder';
 import homepageImage from "../src/homepage.jpg";
@@ -11,12 +12,15 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Placeholder 
-       imageSrc={homepageImage}
-       imageAlt="Placeholder Image"
-      />
-      {/* <Visualization /> */}
+      <Placeholder imageSrc={homepageImage} imageAlt="Placeholder Image" />
+      <Visualization />
       <Prediction />
+      {/* <Router>
+        <Routes>
+            <Route path='/Visualization' element={<Visualization/>} /> 
+            <Route path='/Prediction' element={<Prediction />} /> 
+        </Routes>  
+      </Router> */}
     </div>
   );
 }
---
+++ mental_leo/src/Visualization/Visualization.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Visualization = () => {
+    return (
+        <div background-color="#9b1b6a">
+            <h1><b>WELCOME TO THE VISUALIZATION PAGE</b></h1>
+            <div>
+                <h3>Visualize The Data Whether How Many Number Of People Treated or Not By Genderwise</h3>
+            </div>
+            <div>
+                <h3>Visualize The Data Whether How Many Number Of People Treated or Not By Countrywise</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Visualization;
